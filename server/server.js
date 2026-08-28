8fbe79d8b3904691c815c335bc3d46978d01cc73
Ethantobot <ethantobot7@gmail.com>
2026-08-28 04:02:28 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -339,4 +339,4 @@ app.post('/api/auth/discord', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
