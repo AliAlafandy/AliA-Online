6cebd491cf845ee3c12a8256bf285e228b1b82e8
Ethantobot <ethantobot7@gmail.com>
2026-08-31 01:29:52 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SECRET_KEY);
 const BUCKET_NAME = 'mods-bucket';
 
 mongoose.connect(process.env.MONGO_URI)
